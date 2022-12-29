9917df2247c52df8156e7aa4809fd26a09304cd5
mouseorcleg <torindubozit@gmail.com>
2022-12-29 12:07:10 +0000

    Initial Commit

---
+++ Inspo/InspoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InspoApp.swift
+//  Inspo
+//
+//  Created by Maria Kharybina on 29/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct InspoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
