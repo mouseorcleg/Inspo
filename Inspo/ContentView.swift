5b7a4c05372ad33e1382bb619e933e365f39dd59
mouseorcleg <torindubozit@gmail.com>
2023-01-05 17:04:03 +0000

    Going back to the state without refreshable

--- Inspo/ContentView.swift
+++ Inspo/ContentView.swift
@@ -14,81 +14,67 @@ struct ContentView: View {
     
     var body: some View {
         
-        ScrollView {
-            ZStack {
-                Color.theme.background
-                    .ignoresSafeArea()
-                
-                VStack {
-                    Spacer()
-                    AsyncImage(url: viewModel.imageURL) { returnedImage in
-                        returnedImage
-                            .resizable()
-                            .scaledToFit()
-                            .cornerRadius(20)
-                            .frame(maxWidth: 500, maxHeight: 400)
-                            .padding(10)
-                        
-                    } placeholder: {
-                        Image("pictureFrame")
-                            .resizable()
-                            .scaledToFit()
-                            .aspectRatio(contentMode: .fit)
-                            .rotationEffect(.degrees(rotation))
-                    }
-                    Spacer()
-                    
-    //                Button {
-    //                    withAnimation(Animation
-    //                        .spring()
-    //                        .repeatCount(1)
-    //                    ) {
-    //                        Task{
-    //                            await viewModel.getPaintingForTheView()
-    //                        }
-    //
-    //                        self.rotation += 360
-    //                    }
-    //
-    //                } label: {
-    //                    ZStack{
-    //                        Circle()
-    //                            .fill(Color.theme.accent)
-    //                            .frame(height: 150)
-    //                            .overlay {
-    //                                Text("Pull for inspo")
-    //                                    .font(.title3)
-    //                                    .foregroundColor(.primary)
-    //                        }
-    //                    }
-    //                }
-                                        ZStack{
-                                            Circle()
-                                                .fill(Color.theme.accent)
-                                                .frame(height: 150)
-                                                .overlay {
-                                                    Text("Pull for inspo")
-                                                        .font(.title3)
-                                                        .foregroundColor(.primary)
-                                            }
-                                        }
-                Spacer()
-                Spacer()
-                Spacer()
-                }
-            }
-        }
-        .refreshable {
-            withAnimation(Animation
-                                    .spring()
-                                    .repeatCount(1)
-                                ) {
-                                    Task{
-                                        await viewModel.getPaintingForTheView()
-                                    }
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
             
-                                    self.rotation += 360
-                                }
+            VStack {
+                Spacer()
+                AsyncImage(url: viewModel.imageURL) { returnedImage in
+                    returnedImage
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(20)
+                        .frame(maxWidth: 500, maxHeight: 400)
+                        .padding(10)
+                    
+                } placeholder: {
+                    Image("pictureFrame")
+                        .resizable()
+                        .scaledToFit()
+                        .aspectRatio(contentMode: .fit)
+                        .rotationEffect(.degrees(rotation))
+                }
+                Spacer()
+                
+//                Button {
+//                    withAnimation(Animation
+//                        .spring()
+//                        .repeatCount(1)
+//                    ) {
+//                        Task{
+//                            await viewModel.getPaintingForTheView()
+//                        }
+//
+//                        self.rotation += 360
+//                    }
+//
+//                } label: {
+//                    ZStack{
+//                        Circle()
+//                            .fill(Color.theme.accent)
+//                            .frame(height: 150)
+//                            .overlay {
+//                                Text("Pull for inspo")
+//                                    .font(.title3)
+//                                    .foregroundColor(.primary)
+//                        }
+//                    }
+//                }
+                                    ZStack{
+                                        Circle()
+                                            .fill(Color.theme.accent)
+                                            .frame(height: 150)
+                                            .overlay {
+                                                Text("Pull for inspo")
+                                                    .font(.title3)
+                                                    .foregroundColor(.primary)
+                                        }
+                                    }
+            Spacer()
+            Spacer()
+            Spacer()
+            }
         }
     }
 }
