a777ac150d468c7e41d57d07064bb352722f38d2
mouseorcleg <torindubozit@gmail.com>
2023-01-05 17:55:26 +0000

    OnGesture Refresh that works - frame rotation

--- Inspo/ContentView.swift
+++ Inspo/ContentView.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    
     @StateObject var viewModel: ContentViewModel = ContentViewModel()
-    @State private var rotation = 0.0
+    @State private var offset: CGSize = .zero
+    
     
     var body: some View {
         
@@ -20,60 +21,67 @@ struct ContentView: View {
             
             VStack {
                 Spacer()
-                AsyncImage(url: viewModel.imageURL) { returnedImage in
-                    returnedImage
-                        .resizable()
-                        .scaledToFit()
-                        .cornerRadius(20)
-                        .frame(maxWidth: 500, maxHeight: 400)
-                        .padding(10)
-                    
-                } placeholder: {
-                    Image("pictureFrame")
-                        .resizable()
-                        .scaledToFit()
-                        .aspectRatio(contentMode: .fit)
-                        .rotationEffect(.degrees(rotation))
-                }
-                Spacer()
                 
-//                Button {
-//                    withAnimation(Animation
-//                        .spring()
-//                        .repeatCount(1)
-//                    ) {
-//                        Task{
-//                            await viewModel.getPaintingForTheView()
-//                        }
-//
-//                        self.rotation += 360
-//                    }
-//
-//                } label: {
-//                    ZStack{
-//                        Circle()
-//                            .fill(Color.theme.accent)
-//                            .frame(height: 150)
-//                            .overlay {
-//                                Text("Pull for inspo")
-//                                    .font(.title3)
-//                                    .foregroundColor(.primary)
-//                        }
-//                    }
-//                }
-                                    ZStack{
-                                        Circle()
-                                            .fill(Color.theme.accent)
-                                            .frame(height: 150)
-                                            .overlay {
-                                                Text("Pull for inspo")
-                                                    .font(.title3)
-                                                    .foregroundColor(.primary)
+                    AsyncImage(url: viewModel.imageURL) { returnedImage in
+                        returnedImage
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(20)
+                            .frame(maxWidth: 500, maxHeight: 400)
+                            .padding(10)
+                    } placeholder: {
+                        ZStack{
+                            ProgressView()
+                            Image("pictureFrame")
+                                .resizable()
+                                .scaledToFit()
+                                .aspectRatio(contentMode: .fit)
+                        }
+                    }
+                
+
+                Spacer()
+    
+                ZStack{
+                    Circle()
+                        .fill(Color.theme.accent)
+                        .frame(height: 150)
+                        .overlay {
+                            Text("Pull for inspo")
+                                .font(.title3)
+                                .foregroundColor(.primary)
+                        }
+                        .offset(offset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ value in
+                                    withAnimation(Animation
+                                        .spring()
+                                    ) {
+                                        Task{
+                                            await viewModel.getPaintingForTheView()
+                                        }
+                                        let translation = value.translation
+                                        if translation.height > 0 {
+                                            let verticalTranslation = min(translation.height, 200)
+                                            withAnimation(.spring()) {
+                                                offset = CGSize(width: 0, height: verticalTranslation)
+                                            }
                                         }
                                     }
-            Spacer()
-            Spacer()
-            Spacer()
+                                })
+                                .onEnded({ value in
+                                    withAnimation {
+                                        offset = .zero
+                                    }
+                                    
+                        
+                            })
+                        )
+                }
+                Spacer()
+                Spacer()
+                Spacer()
             }
         }
     }
